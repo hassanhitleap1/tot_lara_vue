dc5291b2c28a51c34b87f658c303082da87e7de6
hassan kiwan <hassanhitleap1@gmail.com>
2021-09-22 15:22:11 +0300

    lesson 36

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,17 @@ Route::apiResource('/category', 'QuestionController');
 Route::apiResource('/{question}/reply', 'ReplyController');
 Route::post('reply/{like}', 'LikeController@like');
 Route::delete('reply/{like}', 'LikeController@unLike');
+
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+    Route::post('login', 'AuthController@login');
+    Route::post('signup', 'AuthController@signup');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+});
