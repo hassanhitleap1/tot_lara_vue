bc33ec7e49a1fb06e6b35226749f3f6049a3dad6
hassan kiwan <hassanhitleap1@gmail.com>
2021-09-22 17:28:03 +0300

    lesson 36

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,32 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
 require('./bootstrap');
+
+window.Vue = require('vue');
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+Vue.use(Vuetify);
+
+Vue.use(VueSimplemde)
+
+window.Exception = Exception
+
+window.EventBus = new Vue();
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+// Vue.component('AppHome', require('./components/AppHome.vue'));
+// import router from './Router/router.js'
+
+const app = new Vue({
+    el: '#app',
+    router
+});
