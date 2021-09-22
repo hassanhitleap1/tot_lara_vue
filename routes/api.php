b740bcf80a43b6143af29ce1799faba12c3bdc48
hassan kiwan <hassanhitleap1@gmail.com>
2021-09-22 12:41:50 +0300

    lesson 28

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,5 @@
 Route::apiResource('/question', 'QuestionController');
 Route::apiResource('/category', 'QuestionController');
 Route::apiResource('/{question}/reply', 'ReplyController');
+Route::post('reply/{like}', 'LikeController@like');
+Route::delete('reply/{like}', 'LikeController@unLike');
